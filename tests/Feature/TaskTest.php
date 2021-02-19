fb5bb3aa3e336bb9722547b47a41aa7a061fd923
馬場将毅 <babamasaki@babasusumutakeshinoMacBook-Pro.local>
2021-02-20 01:47:19 +0900

    外部キー制約エラー

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -46,7 +46,7 @@ class TaskTest extends TestCase
         ]);
 
         $response->assertSessionHasErrors([
-            //'due_date' => '期限日 には日付を入力してください。',
+            'due_date' => '期限日 には日付を入力してください。',
         ]);
     }
 
@@ -62,9 +62,26 @@ class TaskTest extends TestCase
         ]);
 
         $response->assertSessionHasErrors([
-            //'due_date' => '期限日 には今日以降の日付を入力してください。',
+            'due_date' => '期限日 には今日以降の日付を入力してください。',
         ]);
     }
 
+    /**
+    * 状態が定義された値ではない場合はバリデーションエラー
+    * @test
+    */
+    public function status_should_be_within_defined_numbers()
+    {
+        $this->seed('TasksTableSeeder');
+        
+        $response = $this->post('/folders/1/tasks/1/edit', [
+            'title' => 'Sample task',
+            'due_date' => Carbon::today()->format('Y/m/d'),
+            'status' => 999,
+        ]);
 
+        $response->assertSessionHasErrors([
+            'status' => '状態 には 未着手、着手中、完了 のいずれかを指定してください。',
+        ]);
+    }
 }
